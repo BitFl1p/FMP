63edfcb4da85f4b88210c035b0c37e7eac4c831e
bucket_bit_man <511097@chrome.escg.ac.uk>
2021-03-30 12:02:23 +0100

    help

---
+++ Assets/Scripts/Universal/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats
+{
+    public float baseRegen;
+    public float maxHealth;
+    public float baseDamage;
+    public float jumpHeight;
+    public float moveSpeed;
+    public int jumpAmount;
+    public float critChance;
+    public float baseReloadTime;
+}
